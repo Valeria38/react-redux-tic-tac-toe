bb4f5b4d5855904fc555c701831d8769e970886c
Valeria Gora <valeriia308@gmail.com>
2019-03-31 16:35:00 +0300

    Updates

---
+++ precache-manifest.2421779273ad23967ac2134080d1ab38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6fc4739d81e3bbdb58e9",
+    "url": "/react-redux-tic-tac-toe/static/js/runtime~main.2103e1bb.js"
+  },
+  {
+    "revision": "dc799b14eb14a72def96",
+    "url": "/react-redux-tic-tac-toe/static/js/main.6a3966bd.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-redux-tic-tac-toe/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "dc799b14eb14a72def96",
+    "url": "/react-redux-tic-tac-toe/static/css/main.e5297b4d.chunk.css"
+  },
+  {
+    "revision": "fde2cff7ba1ad7ac63ed1afc26d69c85",
+    "url": "/react-redux-tic-tac-toe/index.html"
+  }
+];
