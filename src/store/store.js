bf90024c69ec15445044a4de505999bc58ee338f
Valeria Gora <valeriia308@gmail.com>
2019-04-19 17:09:18 +0300

    redux store added

---
+++ src/store/store.js
@@ -0,0 +1,35 @@
+import { createStore } from 'redux';
+
+const ADD_TURN = 'ADD_TURN';
+
+export const templateTurn = Array(9).fill(null);
+
+const initialState = {
+  turns: [
+    ["X", null, null, null, null, null, null, null, null]
+  ],
+  winner: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TURN:
+      const { index, sign } = action.payload;//5, "X"
+
+      const lastTurn = state.turns.length
+        ? state.turns[state.turns.length - 1]
+        : [...templateTurn];
+
+      const currentTurn = [...lastTurn];
+      currentTurn[index] = sign;
+
+      return {
+        ...state,
+        turns: [...state.turns, currentTurn]
+      };
+
+    default: return state;
+  }
+};
+
+export const store = createStore(reducer);
